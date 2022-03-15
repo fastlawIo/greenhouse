69f2ec95138f9ac3d8d6089dbf9cca2fecfa7d64
Taskaeva Olga <OTaskaeva@bell-main.bellintegrator.ru>
2022-03-15 15:53:52 +0300

    update script, add mask files

--- load_pictures.py
+++ load_pictures.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from os import path
 from PIL import Image
 
@@ -6,18 +7,25 @@ from PIL import Image
 def processing_image(image_file: str) -> Image:
 
     img = cv2.imread(image_file)
-    blue, green, red = cv2.split(img)
-    #cv2.imshow('green', green)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_white = np.array([0, 0, 130])
+    upper_white = np.array([180, 110, 255])
+    mask = cv2.inRange(hsv, lower_white, upper_white)
+    #res = cv2.bitwise_and(img, img, mask=mask)
+    #cv2.imshow('res', res)
 
-    blurred = cv2.GaussianBlur(green, (5, 5), 0)
-    thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 355, 5)
-    #cv2.imshow("Gaussian Adaptive Thresholding", thresh)
+    #thresh2 = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY_INV)
+    ret, thresh = cv2.threshold(mask, 50, 255, cv2.THRESH_BINARY_INV)
+    cv2.imshow('thresh2', thresh)
+
     file_name = path.splitext(image_file)
     cv2.imwrite(str(file_name[0])+'_mask.jpg', thresh)
 
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     return img
 
-processing_image('C:\git\plant-segmentation-my\dataset\\resources\photo_2022-02-23_15-04-49.jpg')
+#processing_image('C:\git\plant-segmentation-my\dataset\\resources\photo_2022-02-23_15-04-44.jpg')
+#processing_image('C:\git\plant-segmentation-my\dataset\\resources\photo_2022-02-23_15-04-52.jpg')
+processing_image('C:\git\plant-segmentation-my\dataset\\resources\photo_2022-02-22_10-21-34.jpg')
